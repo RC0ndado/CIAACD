6eb3d743e2afbebc2f6da1f158b4855fbe848672
Paula Santoyo <69481107+A01745312@users.noreply.github.com>
2023-09-12 13:05:17 -0600

    Rutas y conexion BD

--- ejemploEstrAppFlask/website/src/controllers/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request
-
-# Singleton
-views = Blueprint('views', __name__)
-
-@views.route('/',  methods=['GET'])
-def home():
-    return render_template("home.html")
