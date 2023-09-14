c9ff335019cc2f05686ce492c25fef5944b5bb46
Paula Santoyo <69481107+A01745312@users.noreply.github.com>
2023-09-14 12:17:00 -0600

    Bye carpeta front

--- Front/src/config/index.js
+++
@@ -1,8 +0,0 @@
-// Nombres de acuerdo al estandar de React
-// https://medium.com/how-to-react/using-env-file-in-react-js-b2714235e77e
-export const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
-export const PUBLIC_URL =
-process.env.REACT_APP_PUBLIC_URL || "http://localhost:3000";
-export const API_FLASK_URL =
-process.env.REACT_APP_API_FLASK_URL || "http://localhost:5000";
